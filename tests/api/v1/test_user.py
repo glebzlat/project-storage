4e80035c0ba8d935d496257a80bfb46685e419bb
glebzlat <gleb.zlatanov@gmail.com>
2026-08-08 06:28:24 +0500

    test: /users/token endpoint

--- tests/api/v1/test_user.py
+++ tests/api/v1/test_user.py
@@ -1,18 +1,25 @@
 import uuid
 import pytest
+import jwt
 
 from unittest import mock
 from http import HTTPStatus
+from datetime import timedelta, datetime, timezone
 
 from fastapi.testclient import TestClient
 
 from project_storage.main import app
 from project_storage.core.config import settings
-from project_storage.dependencies import get_register_user_uc
+from project_storage.dependencies import (
+    get_register_user_uc,
+    get_authenticate_user_uc
+)
 from project_storage.use_cases.register_user import (
     RegisteredUser,
     UsernameAlreadyTakenError
 )
+from project_storage.use_cases.authenticate_user import AuthenticateUserUseCase
+from project_storage.models import User
 
 
 @pytest.fixture
@@ -56,3 +63,54 @@ def test_register_user_duplicate_returns_409(test_client):
     assert response.status_code == HTTPStatus.CONFLICT
     assert response.json() == {"detail": "Username already taken"}
     use_case_mock.execute.assert_called_once()
+
+
+def test_login_user(test_client, mocker):
+    password = "password123"
+    user = User(username="johndoe", name="John Doe")
+    repo_mock = mock.Mock()
+    use_case = AuthenticateUserUseCase(repo_mock)
+    mocker.patch.object(use_case, "authenticate", return_value=user)
+    app.dependency_overrides[get_authenticate_user_uc] = lambda: use_case
+
+    now = datetime.now(timezone.utc)
+    expire = now + timedelta(minutes=settings.JWT_EXPIRATION_TIME_MINUTES)
+    jwt_data = {
+        "sub": user.username,
+        "name": user.name,
+        "exp": expire,
+        "iat": now
+    }
+    expected_jwt = jwt.encode(
+        jwt_data,
+        settings.JWT_SECRET_KEY,
+        algorithm=settings.JWT_ALGORITHM
+    )
+
+    response = test_client.post(
+        f"{settings.API_PATH}/users/token",
+        data={"username": user.username, "password": password}
+    )
+
+    response_data = response.json()
+
+    assert response_data["token_type"] == "bearer"
+    assert response_data["access_token"] == expected_jwt
+
+
+def test_login_nonexistent_user_returns_401(test_client, mocker):
+    password = "password123"
+    user = User(username="johndoe", name="John Doe")
+    repo_mock = mock.Mock()
+    use_case = AuthenticateUserUseCase(repo_mock)
+    mocker.patch.object(use_case, "authenticate", return_value=None)
+    app.dependency_overrides[get_authenticate_user_uc] = lambda: use_case
+
+    response = test_client.post(
+        f"{settings.API_PATH}/users/token",
+        data={"username": user.username, "password": password}
+    )
+
+    assert response.status_code == HTTPStatus.UNAUTHORIZED
+    assert response.headers.get("WWW-Authenticate") == "Bearer"
+    assert response.json() == {"detail": "Incorrect username or password"}
